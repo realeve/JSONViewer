c709c40e01e51c1c6fb7c3fb67e3b9b0b5cd4d65
realeve <realeve@qq.com>
2020-05-07 10:25:59 +0800

    update build config

--- .umirc.ts
+++ .umirc.ts
@@ -1,33 +1,47 @@
 import { IConfig } from 'umi-types';
 
 // ref: https://umijs.org/config/
-const config: IConfig =  {
+const config: IConfig = {
   treeShaking: true,
+  publicPath: './',
+  history: 'hash',
+  hash: true, //添加hash后缀
+  exportStatic: false,
+  targets: {
+    ie: 10,
+    chrome: 47,
+    firefox: 40,
+    ios: 7,
+    android: 4,
+  },
+  cssnano: {
+    mergeRules: false,
+  },
+  autoprefixer: { flexbox: true },
   routes: [
     {
       path: '/',
       component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
+      routes: [{ path: '/', component: '../pages/index' }],
+    },
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: false,
-      dynamicImport: false,
-      title: 'JSONViewer',
-      dll: false,
-      
-      routes: {
-        exclude: [
-          /components\//,
-        ],
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: false,
+        dynamicImport: false,
+        title: 'JSONViewer',
+        dll: false,
+
+        routes: {
+          exclude: [/components\//],
+        },
       },
-    }],
+    ],
   ],
-}
+};
 
 export default config;
